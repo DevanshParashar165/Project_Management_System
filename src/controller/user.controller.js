cd00f0995cd91082f669ab8b707c9768d94f9e24
DevanshParashar165 <parashardevansh165@gmail.com>
2025-08-28 23:44:41 +0530

    logout and Current user fetched functialities added

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -4,50 +4,50 @@ import { ApiError } from "../utils/ApiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { emailVerificationMailgenContent, sendEmail } from "../utils/mail.js";
 
-const generateAccessAndRefreshTokens = async(userId) => {
+const generateAccessAndRefreshTokens = async (userId) => {
     try {
         const user = await User.findById(userId)
         const accessToken = user.generateAccessToken()
         const refreshToken = user.generateRefreshToken()
 
         user.refreshToken = refreshToken;
-        await user.save({validateBeforeSave : false})
-        return {accessToken,refreshToken}
+        await user.save({ validateBeforeSave: false })
+        return { accessToken, refreshToken }
     } catch (error) {
-        throw new ApiError(500,"Something went wrong while generating Access Token")
+        throw new ApiError(500, "Something went wrong while generating Access Token")
     }
 }
 
-const registerUser = asyncHandler(async(req,res)=> {
-    const {email,password,username,role} = req.body
+const registerUser = asyncHandler(async (req, res) => {
+    const { email, password, username, role } = req.body
 
     const existedUser = await User.findOne({
-        $or : [{username},{email}]
+        $or: [{ username }, { email }]
     })
 
-    if(existedUser){
-        throw new ApiError(409,"User with email or username already exists",[])
+    if (existedUser) {
+        throw new ApiError(409, "User with email or username already exists", [])
     }
 
     const user = await User.create({
         email,
         password,
         username,
-        isEmailVerified : false
+        isEmailVerified: false
     })
 
-    const {unHashedToken,hashedToken,tokenExpiry} = user.generateTemporaryToken();
+    const { unHashedToken, hashedToken, tokenExpiry } = user.generateTemporaryToken();
 
     user.emailVerificationToken = hashedToken;
     user.emailVerificationExpiry = tokenExpiry;
 
-    await user.save({validateBeforeSave : false})
+    await user.save({ validateBeforeSave: false })
 
     await sendEmail(
         {
-            email : user.email,
-            subject : "Please Verify your email",
-            mailgenContent : emailVerificationMailgenContent(
+            email: user.email,
+            subject: "Please Verify your email",
+            mailgenContent: emailVerificationMailgenContent(
                 user.username,
                 `${req.protocol}://${req.get("host")}/api/v1/users/verify-email/${unHashedToken}`
             )
@@ -55,69 +55,103 @@ const registerUser = asyncHandler(async(req,res)=> {
     )
 
     const createdUser = await User.findById(user._id)
-  .select("-password -refreshToken -emailVerificationToken -emailVerificationExpiry");
+        .select("-password -refreshToken -emailVerificationToken -emailVerificationExpiry");
 
-    if(!createdUser){
-        throw new ApiError(500,"Something went wrong while registering user")
+    if (!createdUser) {
+        throw new ApiError(500, "Something went wrong while registering user")
     }
 
     return res
-              .status(200)
-              .json(
-                new ApiResponse(
-                    200 , {user : createdUser} , "User registered successfully and verification email has been sent on your mail"
-                )
-              )
+        .status(200)
+        .json(
+            new ApiResponse(
+                200, { user: createdUser }, "User registered successfully and verification email has been sent on your mail"
+            )
+        )
 })
 
-const loginUser = asyncHandler(async(req,res)=>{
-    const {email,password} = req.body;
-    
-    if(!email && !password){
-        throw new ApiError(400,"Email or password is not available!!!")
+const loginUser = asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email && !password) {
+        throw new ApiError(400, "Email or password is not available!!!")
     }
 
-    const user = await User.findOne({email})
+    const user = await User.findOne({ email })
 
-    if(!user){
-        throw new ApiError(400,"User does no exist")
+    if (!user) {
+        throw new ApiError(400, "User does no exist")
     }
 
     const isPasswordValid = await user.isPasswordCorrect(password)
 
-    if(!isPasswordValid){
-        throw new ApiError(400,"Invalid Credentials")
+    if (!isPasswordValid) {
+        throw new ApiError(400, "Invalid Credentials")
     }
 
-    const {accessToken,refreshToken} = await generateAccessAndRefreshTokens(user._id)
+    const { accessToken, refreshToken } = await generateAccessAndRefreshTokens(user._id)
 
-     const loggedInUser = await User.findById(user._id)
-  .select("-password -refreshToken -emailVerificationToken -emailVerificationExpiry");
+    const loggedInUser = await User.findById(user._id)
+        .select("-password -refreshToken -emailVerificationToken -emailVerificationExpiry");
 
-    if(!loggedInUser){
-        throw new ApiError(500,"Something went wrong while registering user")
+    if (!loggedInUser) {
+        throw new ApiError(500, "Something went wrong while registering user")
     }
 
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res
+        .status(200)
+        .cookie("accessToken", accessToken, options)
+        .cookie("refreshToken", refreshToken, options)
+        .json(
+            new ApiResponse(
+                200,
+                {
+                    user: loggedInUser,
+                    accessToken,
+                    refreshToken
+                },
+                "User logged In successfully"
+            )
+        )
+
+})
+
+const logoutUser = asyncHandler(async (req, res) => {
+    await User.findByIdAndUpdate(req.user._id, {
+        $set: {
+            refreshToken: ""
+        }
+    },
+        {
+            new: true
+        }
+    )
     const options = {
         httpOnly : true,
         secure : true
     }
 
-    return res
-             .status(200)
-             .cookie("accessToken",accessToken,options)
-             .cookie("refreshToken",refreshToken,options)
-             .json(
-                new ApiResponse(
-                    200,
-                    {user : loggedInUser,
-                        accessToken,
-                        refreshToken
-                    },
-                    "User logged In successfully"
-                )
-             )
-
+    res.status(200)
+       .clearCookie("accessToken",options)
+       .clearCookie("refreshToken",options)
+       .json(
+        new ApiResponse(200,{},"User logged out succesfully")
+       )
 })
 
-export {registerUser,loginUser}
+const getCurrentUser = asyncHandler(async(req,res) => {
+    return res.status(200)
+              .json(
+                new ApiResponse(
+                    200 , req.user,"Current user fetched succesfully"
+                )
+              )
+})
+
+
+export { registerUser, loginUser, logoutUser,getCurrentUser }
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
-import { loginUser, registerUser } from "../controller/user.controller.js";
+import { loginUser, logoutUser, registerUser, getCurrentUser } from "../controller/user.controller.js";
 import { validate } from "../middleware/validator.middleware.js";
-import { userRegisterValidator,userLoginValidator } from "../validators/index.js";
+import { userRegisterValidator,userLoginValidator} from "../validators/index.js";
+import { verifyJWT } from "../middleware/auth.middleware.js";
 
 const authRouter = Router()
 
 authRouter.route('/register').post(userRegisterValidator(),validate,registerUser)
 authRouter.route('/login').post(userLoginValidator(),validate,loginUser)
+authRouter.route('/logout').post(verifyJWT,logoutUser)
 
 export default authRouter
